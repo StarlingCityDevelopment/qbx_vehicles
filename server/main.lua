d9ac46d4cbb068f7c1f1460aaea50eb1b03cdb3c
Codexis <contact@codexis.dev>
2026-03-18 16:44:41 +0100

    feat: add group field to player_vehicles

--- server/main.lua
+++ server/main.lua
@@ -18,7 +18,7 @@ local triggerEventHooks = require '@qbx_core.modules.hooks'
 ---@param plate string
 ---@return boolean
 local function doesEntityPlateExist(plate)
-    local result = MySQL.scalar.await('SELECT 1 FROM player_vehicles WHERE plate = ? LIMIT 1', {plate})
+    local result = MySQL.scalar.await('SELECT 1 FROM player_vehicles WHERE plate = ? LIMIT 1', { plate })
     return result ~= nil
 end
 
@@ -27,6 +27,7 @@ exports('DoesPlayerVehiclePlateExist', doesEntityPlateExist)
 ---@class PlayerVehicle
 ---@field id number
 ---@field citizenid? string
+---@field group? string
 ---@field modelName string
 ---@field garage string
 ---@field state State
@@ -36,6 +37,7 @@ exports('DoesPlayerVehiclePlateExist', doesEntityPlateExist)
 
 ---@class PlayerVehiclesFilters
 ---@field citizenid? string
+---@field group? string
 ---@field states? State|State[]
 ---@field garage? string
 
@@ -51,29 +53,33 @@ local function buildWhereClause(filters)
     local placeholders = {}
     local whereClauseCrumbs = {}
     if filters.vehicleId then
-        whereClauseCrumbs[#whereClauseCrumbs+1] = 'id = ?'
-        placeholders[#placeholders+1] = filters.vehicleId
+        whereClauseCrumbs[#whereClauseCrumbs + 1] = 'id = ?'
+        placeholders[#placeholders + 1] = filters.vehicleId
     end
     if filters.citizenid then
-        whereClauseCrumbs[#whereClauseCrumbs+1] = 'citizenid = ?'
-        placeholders[#placeholders+1] = filters.citizenid
+        whereClauseCrumbs[#whereClauseCrumbs + 1] = 'citizenid = ?'
+        placeholders[#placeholders + 1] = filters.citizenid
+    end
+    if filters.group then
+        whereClauseCrumbs[#whereClauseCrumbs + 1] = 'group = ?'
+        placeholders[#placeholders + 1] = filters.group
     end
     if filters.garage then
-        whereClauseCrumbs[#whereClauseCrumbs+1] = 'garage = ?'
-        placeholders[#placeholders+1] = filters.garage
+        whereClauseCrumbs[#whereClauseCrumbs + 1] = 'garage = ?'
+        placeholders[#placeholders + 1] = filters.garage
     end
     if filters.states then
         if type(filters.states) ~= 'table' then
             ---@diagnostic disable-next-line: assign-type-mismatch
-            filters.states = {filters.states}
+            filters.states = { filters.states }
         end
         if #filters.states > 0 then
             local statePlaceholders = {}
             for i = 1, #filters.states do
-                placeholders[#placeholders+1] = filters.states[i]
+                placeholders[#placeholders + 1] = filters.states[i]
                 statePlaceholders[i] = 'state = ?'
             end
-            whereClauseCrumbs[#whereClauseCrumbs+1] = string.format('(%s)', table.concat(statePlaceholders, ' OR '))
+            whereClauseCrumbs[#whereClauseCrumbs + 1] = string.format('(%s)', table.concat(statePlaceholders, ' OR '))
         end
     end
 
@@ -83,15 +89,16 @@ end
 ---@param filters? PlayerVehiclesInternalFilters
 ---@return PlayerVehicle[]
 local function getPlayerVehiclesInternal(filters)
-    local query = 'SELECT id, citizenid, vehicle, mods, garage, state, depotprice, coords FROM player_vehicles'
+    local query = 'SELECT id, citizenid, group, vehicle, mods, garage, state, depotprice, coords FROM player_vehicles'
     local whereClause, placeholders = buildWhereClause(filters)
     lib.print.debug(query .. whereClause)
     local results = MySQL.query.await(query .. whereClause, placeholders)
     local ownedVehicles = {}
     for _, data in pairs(results) do
-        ownedVehicles[#ownedVehicles+1] = {
+        ownedVehicles[#ownedVehicles + 1] = {
             id = data.id,
             citizenid = data.citizenid,
+            group = data.group,
             modelName = data.vehicle,
             garage = data.garage,
             state = data.state,
@@ -129,6 +136,7 @@ exports('GetPlayerVehicle', getPlayerVehicle)
 ---@class CreatePlayerVehicleRequest
 ---@field model string model name
 ---@field citizenid? string owner of the vehicle
+---@field group? string group of the vehicle
 ---@field garage? string
 ---@field props? table ox_lib properties to set. See https://github.com/overextended/ox_lib/blob/master/resource/vehicleProperties/client.lua#L3
 
@@ -148,51 +156,60 @@ local function createPlayerVehicle(request)
     props.fuelLevel = props.fuelLevel or 100
     props.model = joaat(request.model)
 
-    if not triggerEventHooks('createPlayerVehicle', { citizenid = request.citizenid, garage = request.garage, props = props }) then
+    if not triggerEventHooks('createPlayerVehicle', { citizenid = request.citizenid, group = request.group, garage = request.garage, props = props }) then
         return nil, {
             code = 'hook_cancelled',
             message = 'a createPlayerVehicle event hook cancelled this operation'
         }
     end
 
-    return MySQL.insert.await('INSERT INTO player_vehicles (license, citizenid, vehicle, hash, mods, plate, state, garage) VALUES ((SELECT license FROM players WHERE citizenid = @citizenid), @citizenid, @vehicle, @hash, @mods, @plate, @state, @garage)', {
-        citizenid = request.citizenid,
-        vehicle = request.model,
-        hash = props.model,
-        mods = json.encode(props),
-        plate = props.plate,
-        state = request.garage and State.GARAGED or State.OUT,
-        garage = request.garage
-    })
+    return MySQL.insert.await(
+        'INSERT INTO player_vehicles (license, citizenid, group, vehicle, hash, mods, plate, state, garage) VALUES ((SELECT license FROM players WHERE citizenid = @citizenid), @citizenid, @group, @vehicle, @hash, @mods, @plate, @state, @garage)',
+        {
+            citizenid = request.citizenid,
+            group = request.group,
+            vehicle = request.model,
+            hash = props.model,
+            mods = json.encode(props),
+            plate = props.plate,
+            state = request.garage and State.GARAGED or State.OUT,
+            garage = request.garage
+        })
 end
 
 exports('CreatePlayerVehicle', createPlayerVehicle)
 
 ---@param vehicleId integer
 ---@param citizenid? string
+---@param group? string
 ---@return boolean success, ErrorResult? errorResult
-local function setPlayerVehicleOwner(vehicleId, citizenid)
+local function setPlayerVehicleOwner(vehicleId, citizenid, group)
     assert(vehicleId ~= nil, "required field vehicleId was nil")
-    if not triggerEventHooks('changeVehicleOwner', { vehicleId = vehicleId, newCitizenId = citizenid }) then
+    if not triggerEventHooks('changeVehicleOwner', { vehicleId = vehicleId, newCitizenId = citizenid, newGroup = group }) then
         return false, {
             code = 'hook_cancelled',
             message = 'a changeVehicleOwner event hook cancelled this operation'
         }
     end
-    MySQL.update.await('UPDATE player_vehicles SET citizenid = @citizenid, license = (SELECT license FROM players WHERE citizenid = @citizenid) WHERE id = @id', {
-        citizenid = citizenid,
-        id = vehicleId
-    })
+    MySQL.update.await(
+        'UPDATE player_vehicles SET citizenid = @citizenid, group = @group, license = (SELECT license FROM players WHERE citizenid = @citizenid) WHERE id = @id',
+        {
+            citizenid = citizenid,
+            group = group,
+            id = vehicleId
+        })
     return true
 end
 
 exports('SetPlayerVehicleOwner', setPlayerVehicleOwner)
 
----@param idType 'citizenid'|'license'|'plate'|'vehicleId'
+---@param idType 'citizenid'|'group'|'license'|'plate'|'vehicleId'
 ---@param idValue string | number
 ---@return boolean success, ErrorResult? errorResult
 local function deletePlayerVehicles(idType, idValue)
-    assert(idType == 'citizenid' or idType == 'license' or idType == 'plate' or idType == 'vehicleId', json.encode(idType) .. ' is not a valid idType')
+    assert(
+        idType == 'citizenid' or idType == 'group' or idType == 'license' or idType == 'plate' or idType == 'vehicleId',
+        json.encode(idType) .. ' is not a valid idType')
 
     local column = idType == 'vehicleId' and 'id' or idType
     MySQL.query.await('DELETE FROM player_vehicles WHERE ' .. column .. ' = ?', {
@@ -229,51 +246,51 @@ local function buildSaveVehicleQuery(vehicleId, options)
     local placeholders = {}
 
     if options.state then
-        crumbs[#crumbs+1] = 'state = ?'
-        placeholders[#placeholders+1] = options.state
+        crumbs[#crumbs + 1] = 'state = ?'
+        placeholders[#placeholders + 1] = options.state
     end
 
     if options.depotPrice then
-        crumbs[#crumbs+1] = 'depotprice = ?'
-        placeholders[#placeholders+1] = options.depotPrice
+        crumbs[#crumbs + 1] = 'depotprice = ?'
+        placeholders[#placeholders + 1] = options.depotPrice
     end
 
     if options.garage then
-        crumbs[#crumbs+1] = 'garage = ?'
-        placeholders[#placeholders+1] = options.garage
+        crumbs[#crumbs + 1] = 'garage = ?'
+        placeholders[#placeholders + 1] = options.garage
     end
 
     if options.props then
-        crumbs[#crumbs+1] = 'mods = ?'
-        placeholders[#placeholders+1] = json.encode(options.props)
+        crumbs[#crumbs + 1] = 'mods = ?'
+        placeholders[#placeholders + 1] = json.encode(options.props)
 
         if options.props.plate then
-            crumbs[#crumbs+1] = 'plate = ?'
-            placeholders[#placeholders+1] = options.props.plate
+            crumbs[#crumbs + 1] = 'plate = ?'
+            placeholders[#placeholders + 1] = options.props.plate
         end
 
         if options.props.fuelLevel then
-            crumbs[#crumbs+1] = 'fuel = ?'
-            placeholders[#placeholders+1] = options.props.fuelLevel
+            crumbs[#crumbs + 1] = 'fuel = ?'
+            placeholders[#placeholders + 1] = options.props.fuelLevel
         end
 
         if options.props.engineHealth then
-            crumbs[#crumbs+1] = 'engine = ?'
-            placeholders[#placeholders+1] = options.props.engineHealth
+            crumbs[#crumbs + 1] = 'engine = ?'
+            placeholders[#placeholders + 1] = options.props.engineHealth
         end
 
         if options.props.bodyHealth then
-            crumbs[#crumbs+1] = 'body = ?'
-            placeholders[#placeholders+1] = options.props.bodyHealth
+            crumbs[#crumbs + 1] = 'body = ?'
+            placeholders[#placeholders + 1] = options.props.bodyHealth
         end
     end
 
     if options.coords then
-        crumbs[#crumbs+1] = 'coords = ?'
-        placeholders[#placeholders+1] = json.encode(options.coords)
+        crumbs[#crumbs + 1] = 'coords = ?'
+        placeholders[#placeholders + 1] = json.encode(options.coords)
     end
 
-    placeholders[#placeholders+1] = vehicleId
+    placeholders[#placeholders + 1] = vehicleId
 
     return string.format('UPDATE player_vehicles SET %s WHERE id = ?', table.concat(crumbs, ',')), placeholders
 end
@@ -297,3 +314,8 @@ local function saveVehicle(vehicle, options)
 end
 
 exports('SaveVehicle', saveVehicle)
+
+Citizen.CreateThreadNow(function()
+    local sql = LoadResourceFile(cache.resource, 'vehicles.sql')
+    MySQL.query.await(sql)
+end)
